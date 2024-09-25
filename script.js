54a10fb97830407aa076ebd93c802a6d22e81653
Thepotii <thepotiiunic@gmail.com>
2024-09-25 17:02:07 -0400

    empezamos el script

---
+++ script.js
@@ -0,0 +1,11 @@
+let persona = {
+    name: 'Rodrigo',
+    lastname: 'Suarez',
+    age: 5,
+    hair: '#000',
+    address: {
+        city: 'Miami',
+        country: 'USA',
+        street: 'develop street'
+    }
+}
